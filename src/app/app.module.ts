1da089e15aae4239daf2d5b68d22cc66aa00fb1b
patricio-mendoza <patriciomendozapasapera@gmail.com>
2023-04-16 13:18:03 -0600

    Create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BodyComponent } from './body/body.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { HomeComponent } from './home/home.component';
+import { ReservacionesComponent } from './reservaciones/reservaciones.component';
+import { AvisosComponent } from './avisos/avisos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BodyComponent,
+    SidenavComponent,
+    HomeComponent,
+    ReservacionesComponent,
+    AvisosComponent
   ],
   imports: [
     BrowserModule,
