f342403a0d32e7d909bcf17d81fbf7721e35bc10
patricio-mendoza <patriciomendozapasapera@gmail.com>
2023-04-20 07:21:20 -0600

    remove import trash

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 import { matriculasValidas } from './matriculas';
 import { Router } from '@angular/router';
 
-import { globals } from '../globals';
-
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
