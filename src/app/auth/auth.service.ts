b813c2aa538f4fa49a07aacc5430b560fe519090
patricio-mendoza <patriciomendozapasapera@gmail.com>
2023-04-25 23:44:08 -0600

    cleanup

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,9 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { Observable, of } from 'rxjs';
-import { tap, delay } from 'rxjs/operators';
-
 const API_URI = 'http://localhost:8888/api';
 
 @Injectable({
