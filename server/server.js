3f4f951330223f621eb47efa3db02c2c0fc60898
patricio-mendoza <patriciomendozapasapera@gmail.com>
2023-05-03 15:26:47 -0600

    cleanup

--- server/server.js
+++ server/server.js
@@ -131,7 +131,6 @@ server.get('/api/instalacion/horario/:id', (req, res) => {
     numDia = dia.getDate() === 0 ? 7 : dia.getDate();
 
     let sql = `SELECT HOUR(ho.hora_apertura) as apertura, HOUR(ho.hora_cierre) as cierre FROM Horario ho JOIN Instalacion ins ON ins.id_instalacion = ho.id_instalacion JOIN Espacio es ON ins.id_instalacion = es.id_instalacion WHERE es.id_espacio=${id} AND dia=${numDia}`
-    console.log(sql);
     db.query(sql, function (error, result) {
         if (error) console.log("Error")
         else res.send({ data: result });
