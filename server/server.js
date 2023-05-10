13998890eb69f51b3f34bff76cf8e08f6bfe3e60
patricio-mendoza <patriciomendozapasapera@gmail.com>
2023-05-10 17:41:32 -0600

    solve bugs

--- server/server.js
+++ server/server.js
@@ -198,9 +198,10 @@ server.post('/api/reservar/espacio', (req, res) => {
 server.get('/api/instalacion/horario/:id', (req, res) => {
     let id = req.params.id;
     let dia = new Date();
-    numDia = dia.getDate() === 0 ? 7 : dia.getDate();
+    numDia = dia.getDay() === 0 ? 7 : dia.getDay();
 
     let sql = `SELECT es.nombre as nombre, HOUR(ho.hora_apertura) as apertura, HOUR(ho.hora_cierre) as cierre FROM Horario ho JOIN Instalacion ins ON ins.id_instalacion = ho.id_instalacion JOIN Espacio es ON ins.id_instalacion = es.id_instalacion WHERE es.id_espacio=${id} AND dia=${numDia}`
+    console.log(sql);
     db.query(sql, function (error, result) {
         if (error) console.log("Error")
         else res.send({ data: result });
