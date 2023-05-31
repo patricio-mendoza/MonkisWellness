21d8b19c17e4f03d5931e2a61df7cfc4d0b1ae07
patricio-mendoza <patriciomendozapasapera@gmail.com>
2023-05-31 16:27:55 -0600

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,6 @@ import { MatInputModule } from '@angular/material/input';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
